e80d0eabb11c623dd115233abd9cbf1dfe1ad46d
shenhua <shenhua@ihuman.com>
2021-10-12 11:31:53 +0800

    audio+plane.cs

---
+++ Scripts/Plane.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plane : MonoBehaviour
+{
+    private int direction = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.y >= 1) direction = -1;
+        if (this.transform.position.y <= -1) direction = 1;
+        Vector3 pos = this.transform.position;
+        this.transform.position = new Vector3(pos.x, pos.y + Time.deltaTime * direction, pos.z);
+    }
+}
