c21227df3ca19832af2ac957a38dc39b8b3b2183
shenhua <shenhua@ihuman.com>
2021-10-15 14:34:44 +0800

    book page curl

--- Scripts/Plane.cs
+++ Scripts/Plane.cs
@@ -2,21 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Plane : MonoBehaviour
+namespace UnityPractice
 {
-    private int direction = 1;
-    // Start is called before the first frame update
-    void Start()
+    public class Plane : MonoBehaviour
     {
+        private int direction = 1;
+        // Start is called before the first frame update
+        void Start()
+        {
         
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (this.transform.position.y >= 1) direction = -1;
-        if (this.transform.position.y <= -1) direction = 1;
-        Vector3 pos = this.transform.position;
-        this.transform.position = new Vector3(pos.x, pos.y + Time.deltaTime * direction, pos.z);
+        // Update is called once per frame
+        void Update()
+        {
+            if (this.transform.position.y >= 1) direction = -1;
+            if (this.transform.position.y <= -1) direction = 1;
+            Vector3 pos = this.transform.position;
+            this.transform.position = new Vector3(pos.x, pos.y + Time.deltaTime * direction, pos.z);
+        }
     }
 }
